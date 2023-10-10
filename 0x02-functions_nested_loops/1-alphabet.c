013488d7c7bf1edeecd5567078735e6d8bd60ce7
abdelakhliij <riadakhlij@gmail.com>
2023-10-10 16:07:02 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,16 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase
  */
+
 void print_alphabet(void)
 {
     char letter = 'a';
 
     while (letter <= 'z')
-    {
+{
         _putchar(letter);
         letter++;
-    }
+}
 
     _putchar('\n');
 }
